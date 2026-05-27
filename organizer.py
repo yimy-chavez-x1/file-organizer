1cda1035a81626561de0e503a0ef8054115568fb
Yimy Chavez <yimy.chavez@virginiogomez.cl>
2026-05-26 20:30:49 -0400

    subir archivo

---
+++ organizer.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import shutil 
+
+EXTENSIONES = {
+    ".mp4": "Videos",
+    ".mp3": "Music",
+    ".jpg": "Imagenes",
+    ".png": "Imagenes",
+    ".jpeg": "Imagenes",
+    ".pdf": "Documentos",
+    ".docx": "Documentos",
+}
+
+def organizar_descargas():
+    descargas = Path.home() / "Downloads"
+    for archivo in descargas.iterdir():
+        if archivo.is_file():
+            extension = archivo.suffix.lower()
+            if extension in EXTENSIONES:
+                destino = descargas / EXTENSIONES[extension]
+                destino.mkdir(exist_ok=True)
+                shutil.move(str(archivo), str(destino / archivo.name))
+
+organizar_descargas()
